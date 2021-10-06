3fc922e2a9de480da5e0a216fa38889f139db0f2
Damon-Nunez <damon.nunez@student.uncommonhs.org>
2021-10-06 14:16:20 +0000

    Killing it

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 //1
-function absoluteValue(){
+function absoluteValue(num){
+  if(Number.isInteger(num) === false){
+      console.log("Not a number m8");
+  } else if( num >= 0){
+      console.log(num);
+  } else if(num < 0) {
+      console.log(num * -1);
+  } 
   
 }
-
-
+absoluteValue(-8);
 
 
